f5d13105d6460e6be4904c61814f5393e6a940e9
Innu Thapa <innuthapa93@gmail.com>
2016-02-09 23:35:57 -0600

    Create constructor to print name and zip code

--- Hello.java
+++ Hello.java
@@ -5,6 +5,13 @@ Write it three different ways to print the same information.
 
 public class Hello {
 
+	// Method III Begin
+	public Hello(String name, int zip_code) {
+		System.out.println("Name : " + name);
+		System.out.println("Zip Code : " + zip_code);
+
+	}
+
 	// Method II Begin
 	public void name() {
 		System.out.println("Name : Amrit Deep Dhungana");
@@ -18,16 +25,23 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, Amrit");
 
-		// Method I : Print Name and Zip Code
+		// Method I : Print Name and Zip Code 
 		System.out.println("******** Method I **********");
 		System.out.println("Name : Amrit Deep Dhungana");
 		System.out.println("Zip Code : 75038");
 
-		// Method II : 
+		// Method II :  Create function or method to print name and zip code
 		System.out.println("******** Method II **********");
-		Hello hello = new Hello();
+		// Hello hello = new Hello();
+		Hello hello = new Hello("Amrit Deep Dhungana", 75038);
+
 		hello.name();
 		hello.zip_code();
+
+		// Method III : Create constructor to print name and zip code
+		System.out.println("******** Method III **********");
+		Hello hellos = new Hello("Amrit Deep Dhungana", 75038);
+
 
 	}
 }
