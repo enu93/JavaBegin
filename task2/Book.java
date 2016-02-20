0491deacb11f9e025513104302014324f6bc5730
Innu Thapa <innuthapa93@gmail.com>
2016-02-20 11:16:08 -0600

    Define construtor for Book class with its attributes

--- task2/Book.java
+++ task2/Book.java
@@ -8,7 +8,16 @@ public class Book {
 	String publisher;
 	int price;
 
+	public Book(String title, String author, int isbn, String publisher, int price) {
+		title = title;
+		author = author;
+		isbn = isbn;
+		publisher = publisher;
+		price = price;
+	}
+
 	public static void main(String[] args) {
+		System.out.println("Hello");
 
 	}
 }
