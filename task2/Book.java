8535ec600ce38ecd658aff24e8f2bfcc558b1374
Innu Thapa <innuthapa93@gmail.com>
2016-02-20 11:10:33 -0600

    create Book class with main function

---
+++ task2/Book.java
@@ -0,0 +1,8 @@
+// 2.	Write Book class with attributes, title, author, isbn, publisher, and price.  
+// Book class has method calculateCharge that accepts the quantity sold and returns the total charge
+
+public class Book {
+	public static void main(String[] args) {
+		
+	}
+}
