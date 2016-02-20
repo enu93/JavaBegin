fea1bc3089a64eba24764b92e7730309f57dd75e
Innu Thapa <innuthapa93@gmail.com>
2016-02-20 11:07:57 -0600

    Create attributes name and state. And called in main function

--- task2/Publisher.java
+++ task2/Publisher.java
@@ -2,7 +2,13 @@
 
 public class Publisher {
 
+	String name = "Jave Beginner";
+	String state = "Texas";
+
 	public static void main(String[] args) {
-		
+		Publisher pub = new Publisher();
+		System.out.println("Name = " + pub.name);
+		System.out.println("State = " + pub.state);
+
 	}
 }
